cc90b850a9103c5cbfd123966ef709f4abcaaf90
Коваленко Вячеслав <vaderadmin@gmail.com>
2016-05-03 15:09:28 +0300

    I continue to work on mods.
    Climate control is now a standalone mod.

---
+++ Climate Control/Source/ClimateControl/PlaceWorker_ClimateControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace ClimateControl
+{
+    // ReSharper disable once UnusedMember.Global
+    // ReSharper disable once InconsistentNaming
+    internal class PlaceWorker_ClimateControl : PlaceWorker
+    {
+        private Building_ClimateControl _climateControl;
+
+        public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot)
+        {
+            foreach (
+                var unit in
+                    Find.ListerBuildings.AllBuildingsColonistOfClass<Building_ClimateControl>()
+                        .Where(unit => unit.Position == center))
+            {
+                _climateControl = unit;
+                break;
+            }
+            var room = RoomQuery.RoomAt(center);
+            if (room == null || room.UsesOutdoorTemperature) return;
+
+            if (_climateControl == null) return;
+            var status = _climateControl.WorkStatus;
+
+            var color = Color.white;
+            switch (status)
+            {
+                case Status.Waiting:
+                    color = new Color(1f, 0.7f, 0.0f, 0.5f);
+                    break;
+                case Status.Heating:
+                    color = GenTemperature.ColorRoomHot;
+                    break;
+                case Status.Cooling:
+                    color = GenTemperature.ColorRoomCold;
+                    break;
+                case Status.Outdoor:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            GenDraw.DrawFieldEdges(room.Cells.ToList(), color);
+        }
+    }
+}
---
+++ Climate Control/Source/ClimateControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Climate Control")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Climate Control")]
+[assembly: AssemblyCopyright("Copyright Vader© 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0c794a67-6298-46cd-924d-54dc03d39b6b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.13.1.0")]
+[assembly: AssemblyFileVersion("0.13.1.0")]
